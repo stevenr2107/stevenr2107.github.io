7e98dce3b25f88a9a597521d5f015492cd72d926
stevenr2107 <110054234+stevenr2107@users.noreply.github.com>
2022-09-23 08:30:37 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,36 @@
 function setup() {
-  createCanvas(windowWidth -20, windowHeight-50);
+  createCanvas(400, 400);
+  richtung = 1;
+  x = 0;
+  y = 0;
 }
 
 function draw() {
   background(220);
+  circle(x, y, 50);
+  fill(3, 50, 110);
+  if (x >= 400) {
+    richtung = 1;
+  }
+  if (x <= 0) {
+    richtung = 0;
+  }
+  if (richtung == 1) {
+    x--;
+  }
+  if (richtung == 0) {
+    x++;
+  }
+  if (y >= 400) {
+    richtung = 1;
+  }
+  if (y <= 0) {
+    richtung = 0;
+  }
+  if (richtung == 1) {
+    y -= 5;
+  }
+  if (richtung == 0) {
+    y += 5;
+  }
 }
