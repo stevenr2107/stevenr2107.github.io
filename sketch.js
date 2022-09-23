1199045bac9664184e418a8027e208f716b4c949
stevenr2107 <110054234+stevenr2107@users.noreply.github.com>
2022-09-23 09:12:10 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+let zBereich;
+
 function setup() {
-  createCanvas(windowWidth -20, windowHeight);
+  zBereich = createCanvas(windowWidth, windowHeight);
+  zBereich.position(0,0); // Links oben
+  zBereich.style("z-index",'-1'); //Verschiebung auf der z-Achse
 }
 
 function draw() {
-  background(220);
+  // Ohne background = transparent
+  //text("Hallo"+mouseX,30,100)
+  noStroke;
+  if(mouseX >windowWidth /2){fill(0,250,0,50)}else{
+    fill(250,0,250,50)
+  }
+  if(mouseY >windowHeight/2){fill(0,100,30,50)}else{
+    fill(100,200,10,50)
+  }
+  //fill(255,50);
+  circle(mouseX,mouseY,20);
 }
